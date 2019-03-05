acbac247bb46802f8d2489327c0b1a5c236fd299
Jusitn Avioli <justin.avioli@gmail.com>
2019-03-05 00:56:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Beatles Loops returns an array of strings containing what instruments each musician plays
 function theBeatlesPlay(musicians, instruments){
-  for(var i=1,i<(musicians.length+1),i++){
-    return musicians.[i]+" plays "+instruments.[i];
+  for(var i=1;i<(musicians.length+1);i++){
+    return musicians[i]+" plays "+instruments[i];
   }
 }
